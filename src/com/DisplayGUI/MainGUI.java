ed40ea1b9555563f8d6d3af98b4bdda8faf8ece1
Arunkumar Mourougappane <amouroug@buffalo.edu>
2024-12-25 18:18:42 -0600

    Update fomatting with formatter

--- src/com/DisplayGUI/MainGUI.java
+++ src/com/DisplayGUI/MainGUI.java
@@ -1,7 +1,6 @@
 
 public class MainGUI {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Initializing Sorting...");
     }
 }
